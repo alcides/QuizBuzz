1440549a8f9ebe4d5f725e7ee085e62979c941f5
Alcides Fonseca <me@alcidesfonseca.com>
2013-04-27 21:37:19 +0100

    Added metro style and removed duplicates

--- server.js
+++ server.js
@@ -5,9 +5,46 @@ var qs = require('querystring');
 
 app.listen(8080);
 
+var originalColors = ['green', 'greenDark','greenLight', 
+    'pink', 'pinkDark', 'yellow', 'darken', 'purple', 
+    'blue', 'blueDark', 'blueLight', 'orange', 'orangeDark',
+    'red'];
+  
+var availableColors = originalColors.slice(0);
 
 var teamOrder = [];
-var dashboardTarget = null
+var teamColors = {};
+var dashboardTarget = null;
+
+
+function addTeam(name) {
+  if (teamOrder.indexOf(name) == -1) {
+    teamOrder.push(name)
+    console.log("New order: " + teamOrder);
+    if (!(name in teamColors)) {
+      if (availableColors.length <= 0) {
+        availableColors = originalColors.slice(0);
+      }
+      teamColors[name] = availableColors.pop(0);
+    }
+  } else {
+    console.log("Repeated order from " + teamOrder);
+  }
+  return {
+    team: teamOrder, 
+    colors: teamColors
+  }
+}
+
+function reset() {
+  console.log("Received a reset command.");
+  teamOrder = [];
+  return {
+    team: teamOrder, 
+    colors: teamColors
+  }
+}
+
 
 
 function handler (req, res) {
@@ -22,9 +59,9 @@ function handler (req, res) {
         return res.end('Error loading index.html');
       }
       if (team_name != "") {
-        teamOrder.push(team_name);
-        console.log("New order: " + teamOrder )
-        if (dashboardTarget != null) dashboardTarget.emit('team', { team:teamOrder });
+        if (dashboardTarget != null) {
+          dashboardTarget.emit('team', addTeam(team_name));
+        }
       }
       res.writeHead(200);
       ndata = data.toString().replace('value=""','value="' + team_name + '"')
@@ -36,9 +73,7 @@ function handler (req, res) {
 io.sockets.on('connection', function (socket) {
   dashboardTarget = socket;
   socket.on('reset', function(data) {
-    console.log("Received a reset command.")
-    teamOrder = [];
-    socket.emit('team', { team:teamOrder });
+    socket.emit('team', reset());
   });
 });
 
